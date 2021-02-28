9ac688ba5889a64e0d54769aa39df9dbbd9858cf
Deberhams_Mac <deberhams@github.com>
2021-02-28 12:32:50 +0300

    Разбираемся с addEventListeners
    addEventListener, this, style, hidden

--- js/script.js
+++ js/script.js
@@ -1,23 +1,51 @@
-let h1 = document.querySelector('h1');
-let button = document.querySelector('button');
-let input = document.querySelector('input');
+// Тема урока: addEventListener, this, style, 
 
-function changeText() { //Функция изменения Заголовка
-    console.log('clicked') //Выводит количество раз нажатых на кнопку
-    h1.textContent = input.value // Изменяет Заголовок на текст введенный внутри поля для ввода
-}
+//-----------------------------------------------------------------------
+// let h1 = document.querySelector('h1');
+// let button = document.querySelector('button');
+// let input = document.querySelector('input');
 
-h1.addEventListener('mouseenter', function(){ //Без именная функция для h1
-    this.style.color = 'purple' // При наведении меняет цвет
-    this.style.backgroundColor = 'yellow' //При наведении меняет фон
-})
+//-----------------------------------------------------------------------
 
-h1.addEventListener('mouseleave', function(){
-    console.log('Mouse left h1') //При отведении от h1 выводит в консоль
-    this.style.color = 'black' //Меняет цвет текста на черный при отведении
-    this.style.backgroundColor = 'transparent' // Менят фон на прозрачный
-})
+// Функция изменяет Заголовок на текст введенный внутри поля для ввода
+// function changeText() { //Функция изменения Заголовка
+//     console.log('clicked') //Выводит количество раз нажатых на кнопку
+//     h1.textContent = input.value // Изменяет Заголовок на текст введенный внутри поля для ввода
+// }
+// button.addEventListener('click', changeText);
+// // Обейзательно вызывать функцию по ссылке т.е без ()
+// // Данный код говорит что при поисхождении события запускай функцию
 
-button.addEventListener('click', changeText);
-// Обейзательно вызывать функцию по ссылке т.е без ()
-// Данный код говорит что при поисхождении события запускай функцию
+//-----------------------------------------------------------------------
+
+// Функция меняет цвет Заголовка при наведений
+// h1.addEventListener('mouseenter', function(){ //Без именная функция для h1
+//     this.style.color = 'purple' // При наведении меняет цвет
+//     this.style.backgroundColor = 'yellow' //При наведении меняет фон
+// })
+
+
+// Функция меняет цвет Заголовка после отведения курсора
+// h1.addEventListener('mouseleave', function(){
+//     console.log('Mouse left h1') //При отведении от h1 выводит в консоль
+//     this.style.color = 'black' //Меняет цвет текста на черный при отведении
+//     this.style.backgroundColor = 'transparent' // Менят фон на прозрачный
+// })
+
+//-----------------------------------------------------------------------
+
+// Решение задач
+// let button = document.querySelector('button')
+// let h1 = document.querySelector('h1')
+
+// Задача 1. Скрыть Заголовок после нажатия на кнопку
+// button.addEventListener('click', function(){
+//     h1.style.color = 'transparent'
+// })
+
+// Задача 2. Скрыть кнопку после нажатия на неё
+// button.addEventListener('click', function(){
+//     this.hidden = true
+// })
+
+//----------------------------------------------------------------
